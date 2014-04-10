d42d1069fc21eeb47e36fcfe1d6760f2a26fa1f0
Gregory BETTON <grebett@e2r7p1.42.fr>
2014-04-10 13:47:59 +0200

    test on inner and outer width and height

---
+++ test.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title></title>
+	<link rel="stylesheet" href="resources/stylesheets/main.css" />
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+</head>
+<body>
+<div id = "main">
+	<button>Hi !</button>
+</div> <!-- #main -->
+
+<!-- TIME FOR SCRIIIIIIIIIIPT -->
+	<script type="text/javascript" charset="utf-8">
+	console.log($(window).width());
+	console.log($(window).height());
+	</script>
+</body>
+</html>
