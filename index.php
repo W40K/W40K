0f17b48c38736c599eb4d8ea5d87f82fd15b4c3f
Gregory BETTON <grebett@e2r7p1.42.fr>
2014-04-10 13:42:04 +0200

    first organisation

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-Welcome !
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title></title>
+	<link rel="stylesheet" href="resources/stylesheets/main.css" />
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+</head>
+<body>
+<div id = "main">
+	<button>Hi !</button>
+</div> <!-- #main -->
+
+<!-- TIME FOR SCRIIIIIIIIIIPT -->
+	<script type="text/javascript" charset="utf-8">
+	$("button").click(function() { alert ("hi"); });
+	</script>
+</body>
+</html>
