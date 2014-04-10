6ef9274775a39f57328d442edae5fa42e110d52a
vgedon <vgedon@student.42.fr>
2014-04-10 12:25:57 +0200

    connection ok

--- install.php
+++ install.php
@@ -7,7 +7,7 @@
 	<body>
 		<form action='?' method='post'>
 			<fieldset>
-				<legend>Connexion a la base de donnée MySQL</legend>
+				<legend>Connection to MySQL database</legend>
 				<span>Login :</span><input type='text' name='login' value='root' /><br />
 				<span>Password :</span><input type='password' name='passwrd' value='' /><br />
 				<input type='submit' name='submit' value='OK' />
@@ -17,7 +17,7 @@
 			<?php
 				if (isset($_POST['login']) and isset($_POST['passwrd']))
 				{
-					echo "<h2>Tentative de connexion a la base de donnée</h2>";
+					echo "<h2>Database connection</h2>";
 					$mysqli = mysqli_connect("local.42.fr", "$_POST[login]", "$_POST[passwrd]");
 					if (mysqli_connect_errno()) {
 						echo "<p>Failed to connect to MySQL: ".mysqli_connect_error()."<p>";
@@ -28,21 +28,30 @@
 						$file = "<?php \$mysqli = mysqli_connect(\"local.42.fr\", \"$_POST[login]\", $_POST[passwrd]\", \"db_W40k\") ?>";
 						file_put_contents("connect.inc.php", $file);
 					}
-					echo "<h2>Creation de la bse de donnée db_W40k</h2>";
+					echo "<h2>db_W40k creation database</h2>";
 					if (mysqli_query($mysqli, "CREATE DATABASE db_W40k") === TRUE)
-						echo "<p>db_W40kt creee avec succes.<br /><p>";
+						echo "<p>db_W40kt succesfully created.<br /><p>";
 					else
-						echo "<p>Erreur a la creation de la base de donnee:".mysqli_error($mysqli)."<p>";
-					if (mysqli_query($mysqli, "CREATE TABLE ship (
-													Ref CHAR(15) NOT NULL,
-													PRIMARY KEY(Ref),
-													Titre CHAR(255),
-													Prix FLOAT,
-													Description TEXT,
-													Cat INT NOT NULL)") === TRUE)
-						echo "<p>catalogue creee avec succes.";
+						echo "<p>Failed to create database:".mysqli_error($mysqli)."<p>";
+					if (mysqli_query($mysqli, "USE db_W40k") === TRUE)
+						echo "<p>Success to switch to db_W40k</p>";
 					else
-						echo "<p>Erreur a la creation de la table:".mysqli_error($mysqli)."<p>";
+						echo "<p>Failed to switch to db_W40k</p>";
+					if (mysqli_query($mysqli, "CREATE TABLE ships (
+						ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+						name VARCHAR(255) NOT NULL,
+						sprite VARCHAR(255) NOT NULL,
+						height INT NOT NULL,
+						width INT NOT NULL,
+						life INT NOT NULL,
+						pp INT NOT NULL,
+						speed INT NOT NULL,
+						agility INT NOT NULL,
+						shield INT NOT NULL,
+						mobilize BOOLEAN DEFAULT TRUE)") === TRUE)
+						echo "<p>Ships table ceated with success.";
+					else
+						echo "<p>Failed to create ships table:".mysqli_error($mysqli)."<p>";
 				}
 			?>
 		</div>
